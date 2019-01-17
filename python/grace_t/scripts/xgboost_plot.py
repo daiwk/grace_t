f305c1b07b0330f5a2d8f54b898d015dacd3d254
daiwk <daiwk@foxmail.com>
2019-01-17 22:51:07 +0800

    x

--- python/grace_t/scripts/xgboost_plot.py
+++ python/grace_t/scripts/xgboost_plot.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+import matplotlib
+matplotlib.use('Agg')
 import xgboost as xgb
 import matplotlib.pyplot as plt
 
