5bef62eb3e559c12f30c8ba05aeaaa51317a3dcb
daiwk <daiwk@foxmail.com>
2019-10-23 02:02:54 +0800

    add note

---
+++ notes/gen_pdf.sh
@@ -0,0 +1,1 @@
+pandoc -N -s --toc --smart --latex-engine=xelatex -V CJKmainfont='Heiti SC' -V mainfont='Times New Roman' -V geometry:margin=1in -f markdown+markdown_in_html_blocks+raw_html-implicit_figures ./rl.md -o ./rl.pdf
---
+++ notes/rl.md
@@ -0,0 +1,7 @@
+
+## 强化学习+推荐
+
+doc很多，生成一个向量，然后用ann
+
+[BEYOND GREEDY RANKING: SLATE OPTIMIZATION VIA LIST-CVAE](https://arxiv.org/pdf/1803.01682.pdf)
+
