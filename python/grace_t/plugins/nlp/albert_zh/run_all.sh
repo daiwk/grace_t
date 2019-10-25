92a8c098faf3c5e03627e99e5071c25014133b43
daiwk <daiwk@foxmail.com>
2019-10-25 23:36:06 +0800

    x

--- python/grace_t/plugins/nlp/albert_zh/run_all.sh
+++ python/grace_t/plugins/nlp/albert_zh/run_all.sh
@@ -4,15 +4,16 @@ source ./source.conf
 
 function create_data()
 {
+export BERT_BASE_DIR=models_pretrain/albert_tiny_489k
 export BERT_BASE_DIR=models_pretrain/albert_tiny_250k
 $python create_pretraining_data.py \
     --do_whole_word_mask=True \
     --input_file=data/news_zh_1.txt \
     --output_file=data/tf_news_2016_zh_raw_news2016zh_1.tfrecord \
-    --vocab_file=$BERT_BASE_DIR/vocab.txt 
+    --vocab_file=$BERT_BASE_DIR/vocab.txt \
     --do_lower_case=True \
     --max_seq_length=128 \
-    --max_predictions_per_seq=51 \
+    --max_predictions_per_seq=128 \
     --masked_lm_prob=0.10
 }
 
@@ -27,7 +28,7 @@ $python run_pretraining.py \
         --bert_config_file=$BERT_BASE_DIR/albert_config_tiny.json \
         --train_batch_size=32 \
         --max_seq_length=128 \
-        --max_predictions_per_seq=51 \
+        --max_predictions_per_seq=128 \
         --num_train_steps=10000 \
         --num_warmup_steps=1000 \
         --learning_rate=0.00176 \
@@ -38,7 +39,7 @@ $python run_pretraining.py \
 function board()
 {
     ps aux| grep tensorboard | grep my_new_model_path| awk '{print $2}'| xargs kill -9 
-    nohup ~/try/python-2.7.8-tf1.4/bin/python ~/try/python-2.7.8-tf1.4/bin/tensorboard --logdir=./my_new_model_path/ --port=8003 &
+    nohup $tensorboard --logdir=./my_new_model_path/ --port=8003 --host=`hostname` &
 }
 
 function main()
